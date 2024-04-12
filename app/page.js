279bfb019d83a29680e9ab696b1fc53f6cc04afd
almustanir <olajidemubarak561@gmail.com>
2024-04-12 08:11:02 +0100

    1

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h2>subscribe to mbtech world</h2>
-    <Button className="bg-red-600">Subscriibe</Button>
+      <Header/>
     </div>
   );
 }
